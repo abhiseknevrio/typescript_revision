82df25b889baaf8bebc9660269e9aa498c3a86bf
abhidubey007 <abhidb95@gmail.com>
2023-07-27 22:21:20 +0530

    Basic Props types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Greet, Person, PersonList } from './components/props';
 
 function App() {
+
+  const personName = {
+    first: 'Abhisek',
+    last: 'Dubey'
+  }
+
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Wayne'
+    },
+    {
+      first: 'Clark',
+      last: 'Kent'
+    },
+    {
+      first: 'Princess',
+      last: 'Diana'
+    }
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
         React with Typescript
+        <Greet name='Abhisek' isLoggedIn={true} />
+        <Person name={personName} />
+        <PersonList names={nameList} />
       </header>
     </div>
   );
